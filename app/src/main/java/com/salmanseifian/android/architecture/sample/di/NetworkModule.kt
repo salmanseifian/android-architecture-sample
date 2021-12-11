8a7944ee2d2b4088adbd32cc27d98a38f6895694
Salman <salman.seifian@gmail.com>
2021-12-11 20:13:36 +0330

    Create network module

---
+++ app/src/main/java/com/salmanseifian/android/architecture/sample/di/NetworkModule.kt
@@ -0,0 +1,48 @@
+package com.salmanseifian.android.architecture.sample.di
+
+import com.salmanseifian.android.architecture.sample.data.remote.ApiService
+import com.salmanseifian.android.architecture.sample.utils.API_BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor() = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor) =
+        OkHttpClient.Builder().addInterceptor(loggingInterceptor)
+            .build()
+
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory = GsonConverterFactory.create()
+
+    @Singleton
+    @Provides
+    fun provideApiServiceService(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): ApiService = Retrofit.Builder()
+        .baseUrl(API_BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(gsonConverterFactory)
+        .build()
+        .create(ApiService::class.java)
+
+}
